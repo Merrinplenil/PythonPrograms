20e8296eedec8d320b6fe01d82cf7f73ac7dc74e
Merrinplenil <57354337+Merrinplenil@users.noreply.github.com>
2025-08-03 19:35:05 +0530

    Add files via upload

---
+++ lemonade.py
@@ -0,0 +1,22 @@
+# bill = [5,5,5,10,20]
+# five=0
+# ten = 0
+# for bill in bill:
+#     if bill == 5:
+#         five = five+1
+#     elif bill == 10:
+#         if five == 0:
+#             return False
+#         else:
+#             five=five-1
+#             ten=ten+1
+#     else:
+#         if ten>0 and five >0:
+#             ten = ten-1
+#             five=five-1
+#         elif five >3:
+#             five = five-3
+#         else:
+#             return False
+# return True
+
